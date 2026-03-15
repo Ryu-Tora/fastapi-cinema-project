c858af0b0e0a93aa8b3010c99f3d810d55440813
Adam Halka <galkaadam4@gmail.com>
2026-03-15 13:04:49 +0100

    Initialising project structure and dependenses

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Online-Cinema",
+    description="Digital platform for movies",
+    version="0.0.1",
+)
+
+@app.get("/")
+def root():
+    return {"status": "success", "message": "Online-Cinema"}
