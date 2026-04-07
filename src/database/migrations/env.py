d3fe692bb68ba8b2a731a91ac61ec5c794e7e355
Adam Halka <galkaadam4@gmail.com>
2026-04-07 15:10:40 +0200

    Debuggin

--- src/database/migrations/env.py
+++ src/database/migrations/env.py
@@ -5,7 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from src.database import Base
+from src.database.models import movies, accounts
+from src.database.models.base import Base
+from src.database.session_postgresql import sync_postgresql_engine
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
